8fa359a73bb0a4105a2b55a00259a1713d403724
Bryan Karta <karta.bryan@gmail.com>
2024-07-01 10:55:07 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="md:h-12 sm:h-4">
+      <h1 className="md:text-8xl sm:text-2xl">Hello World</h1>
+      <div className="clip-path">
+        <img
+          src="https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp"
+          alt="img"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
